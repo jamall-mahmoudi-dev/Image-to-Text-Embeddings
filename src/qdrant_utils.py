3a550143f78c774d6dd7b0a604f178d87a5a5971
Jamal Mahmoudi <jamallmahmoudi2001@gmail.com>
2025-10-19 17:32:16 +0330

    Edit Image-to-Text Embeddings project

--- src/qdrant_utils.py
+++ src/qdrant_utils.py
@@ -1,9 +1,21 @@
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 from config.config import QDRANT_HOST, QDRANT_PORT, COLLECTION_NAME
+import sys
+import os
+
+# add sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 
 def get_qdrant_client():
-    return QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
+    # timeout increase 
+    return QdrantClient(
+        host=QDRANT_HOST,
+        port=QDRANT_PORT,
+        timeout=120  
+    )
+
 
 def create_collection_if_not_exists():
     client = get_qdrant_client()
@@ -14,9 +26,10 @@ def create_collection_if_not_exists():
             collection_name=COLLECTION_NAME,
             vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE)
         )
-        print(f" Created collection '{COLLECTION_NAME}'")
+        print(f"Created collection '{COLLECTION_NAME}'")
     else:
-        print(f" Collection '{COLLECTION_NAME}' already exists.")
+        print(f"Collection '{COLLECTION_NAME}' already exists.")
+
 
 def insert_embedding(vector, caption):
     client = get_qdrant_client()
@@ -26,4 +39,4 @@ def insert_embedding(vector, caption):
             models.PointStruct(id=1, vector=vector, payload={"caption": caption})
         ],
     )
-    print(" Embedding inserted successfully.")
+    print("Embedding inserted successfully.")
