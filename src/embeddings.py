46c4e724933eff56487a816653953054150e922f
Bahram <devops523@gmail.com>
2025-11-13 11:27:28 +0330

    chanage files

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,5 +1,5 @@
 from sentence_transformers import SentenceTransformer
-
+# get all pictuers
 def get_text_embedding(text: str):
     model = SentenceTransformer("all-MiniLM-L6-v2")
     embedding = model.encode(text)
