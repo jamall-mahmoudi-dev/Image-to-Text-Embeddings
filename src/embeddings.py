9dee198dd75afa30e2d7756eabdc638239870392
Bahram <devops523@gmail.com>
2025-11-13 11:38:36 +0330

    add commit project

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer
-# get all pictuers
+# get all git
+#new change file
 def get_text_embedding(text: str):
     model = SentenceTransformer("all-MiniLM-L6-v2")
     embedding = model.encode(text)
